1adaeeacd24da1ff42bad888c9e852d518424303
handyaaa <116147686+handyaaa@users.noreply.github.com>
2023-03-22 10:59:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,15 +51,15 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '7HvHpZSXJErF-GCqPsZeWqOnK7ZTPNZwRQdswlslq8c',
+  //CALLBACK_TEMPLATE_ID: '7HvHpZSXJErF-GCqPsZeWqOnK7ZTPNZwRQdswlslq8c',
 
-  CALLBACK_USERS: [
-    {
-      name: '自己',
+  //CALLBACK_USERS: [
+  //  {
+  //    name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o--gq674EhedMTiXYWiTom7P0XKA',
-    }
-  ],
+  //    id: 'o--gq674EhedMTiXYWiTom7P0XKA',
+  //  }
+ // ],
 
 }
 
