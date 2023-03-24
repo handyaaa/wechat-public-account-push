dc8cd823e9d0e307de7ded82038fc8ccbd086201
handyaaa <116147686+handyaaa@users.noreply.github.com>
2023-03-24 21:03:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,10 +23,10 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o--gq674EhedMTiXYWiTom7P0XKA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'jw9xq3M1qIsRvHOB7k6gWPk3WeI9VXciQClD7ZrUfpg',
+      useTemplateId: 'e6wme4LwN-v0cE8JGPWmpd_kjNNLG5iKgIG6XbuqGT0	',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
-      festivals: [
+      FESTIVALS: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
           type1: '*生日', name: '宝贝', year: '1996', date: '04-09',
@@ -39,7 +39,7 @@ const USER_CONFIG = {
           type3: '节日', name: '相识纪念日', year: '2021', date: '10-02',
         },
       ],
-      festivals_limit: 0,
+      FESTIVALS_LIMIT: 0,
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
